db8cde27de824056a8a47f76c72e0341b69201bf
Ekaterina80git <timofeewa.yekat@yandex.ru>
2024-07-03 23:20:08 +0300

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,41 @@
+# дано- grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+#students ={"Jonni",'Bilbo",'Steve","Khendrik","Aaron"}
+#список садержит оценки учеников в алфавитном порядке множество неупорядочено
+# создать словарь где имя - ключ, значение - средний бал ученика
+
+grades= [[5,3,3,5,4],[2,2,2,3,[4,5,5,2],[4,4,3],[5,5,5,5,4,5]]]
+snudents = {"Jonni","Bilbo","Steve","Khendrik","Aaron"}
+magazin = {"Aaron":[5,3,3,5,4],"Bilbo":[2,2,2,3],"Jonni":[4,5,5,2],"Khendrik":[4,4,3],"Stive":[5,5,5,4,5]}
+list_1 = sum([5,3,3,5,4])
+a = result = (int(list_1//5))
+magazin["Aaron"] = a
+list_2 = sum([2,2,2,3])
+b = result2 = (float(list_2/4))
+magazin["Bilbo"] = b
+list_3 = sum([4,5,5,2])
+c = result3 =(int(list_3//4))
+magazin["Jonni"] = c
+list_4 = sum([4,4,3,])
+d = result4 = (float(list_4/3))
+magazin["Khendrik"] = d
+list_5 = sum([5,5,5,4,5,])
+f = result5 = (float(list_5/5))
+print(result5)
+magazin["Stive"] = f
+print(magazin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
